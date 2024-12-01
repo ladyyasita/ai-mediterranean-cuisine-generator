1356c34ed4cf349b2a74b67c0d1343b8c52dae2f
ladyyasita <121614535+ladyyasita@users.noreply.github.com>
2024-12-01 20:14:39 +0100

    updated changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function displayRecipe(response) {
   new Typewriter("#recipe", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 5,
+    delay: 2,
     cursor: "",
   });
 }
@@ -12,13 +12,13 @@ function generateRecipe(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "c3a451d0adt46fobb2b9a77755f49315";
   let context =
-    "You are the Mediterranean cuisine expert and love to share delicious recipes. The recipe should include a brief description of the dish, a detailed list of ingredients, and step-by-step instructions. When listing ingridients, please separate each line with a <li>. Make sure to follow user instructions. Please include a recipe title separetely inside a <strong> and <h> element at the top";
-  let prompt = `User instructions: Create a Mediterranean recipe about ${instructionsInput.value}`;
+    "You are the Mediterranean cuisine expert and love to share delicious recipes. The recipe should include a brief description of the dish, a detailed list of ingredients, and step-by-step instructions. When listing ingridients, please separate each line with a <li>. Make sure to follow user instructions inside <div>. Please include a recipe title inside a <strong> and <h> element.";
+  let prompt = `User instructions: Create a Mediterranean recipe of ${instructionsInput.value}`;
 
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   let recipeElement = document.querySelector("#recipe");
   recipeElement.classList.remove("hidden");
-  recipeElement.innerHTML = `<div class="generating">⏳  Creating a recipe of ${instructionsInput.value}</div>`;
+  recipeElement.innerHTML = `<div class="generating">⏳ Creating a recipe of ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayRecipe);
 }
